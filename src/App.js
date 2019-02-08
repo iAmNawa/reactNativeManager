15e55df8b5d8377811f8ce3861d5e294f5ba4698
Paul Borawski <paulborawski14@gmail.com>
2019-02-08 09:57:40 -0800

    added firebase and secret login info

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import secrets from './secrets';
 
 export default class HelloWorldApp extends Component {
+  componentWillMount() {
+    firebase.initializeApp(secrets);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
