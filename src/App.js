ba14fb052dc91f5ec3316370672dcecf42b0facd
Paul Borawski <paulborawski14@gmail.com>
2019-02-07 21:04:48 -0800

    added Provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 export default class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: 'white' }}>
-        <Text>Hello world!</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: 'white' }}>
+          <Text>Hello world!</Text>
+        </View>
+      </Provider>
     );
   }
 }
