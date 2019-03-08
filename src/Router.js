c162d96ac36dfdf7b0f67e708adef15ba4492a9b
Paul Borawski <paulborawski14@gmail.com>
2019-03-08 14:28:47 -0800

    fixed a typo

--- src/Router.js
+++ src/Router.js
@@ -22,7 +22,7 @@ const RouterComponent = () => {
             initial
           />
         <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
-        <Scene key="employeeEdit" component={employeeEdit} title="Edit Employee" />
+        <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
         </Scene>
       </Scene>
     </Router>
